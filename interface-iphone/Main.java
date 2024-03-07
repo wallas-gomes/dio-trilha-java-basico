5b81f77eee841f1203dcaff42d7ae36d2a5c6e93
wallas-gomes <gomes.wallas@yahoo.com.br>
2024-03-06 21:59:51 -0300

    interface do iphone

---
+++ interface-iphone/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.selecionarMusica();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    
+    }
+    
+}
---
+++ interface-iphone/Navegador.java
@@ -0,0 +1,5 @@
+public interface Navegador {
+    public void exibirPagina();
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
